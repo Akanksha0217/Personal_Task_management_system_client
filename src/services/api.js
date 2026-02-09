fe0437a2057f419a477e1733f065cb319879af78
Akanksha Baykar <akankshabaykar@gmail.com>
2026-02-09 12:43:28 +0530

    render update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL:"https://personal-task-management-system-server.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
